6c7dbf1c9c7be577410d528eb1aceb212ca8017b
xiaoqiangwang <xiaoqiangwang@gmail.com>
2011-02-09 07:30:11 +0100

    a dummy echo server

---
+++ example/dummy.py
@@ -0,0 +1,17 @@
+from pcas import Driver, SimpleServer
+import time
+
+if __name__ == '__main__':
+    driver = Driver()
+    server = SimpleServer()
+    from db import pvdb, prefix
+    for pvname in pvdb:
+        info = pvdb[pvname]
+        pv = server.createPV(prefix, pvname, info, driver)
+        driver.registerPV(pv)
+
+    while True:
+        # process CA transactions
+        server.process(0.01)
+        # give other thread a chance
+        time.sleep(0.01)
