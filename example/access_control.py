63521c1e9650e33de4fa100e46469c7baa092912
xiaoqiangwang <xiaoqiangwang@gmail.com>
2011-09-26 17:04:30 +0200

    access security example

---
+++ example/access_control.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from pcaspy import Driver, SimpleServer, asInitFile, asCaStart
+import time
+
+prefix = 'MTEST:'
+pvdb = {
+    'LEVEL': {
+    },
+    'FILL' : {
+        'prec' : 3,
+        'asg'  : 'fill',
+    },
+}
+
+class myDriver(Driver):
+    def  __init__(self):
+        super(myDriver, self).__init__()
+
+if __name__ == '__main__':
+    server = SimpleServer()
+    server.initAccessSecurityFile('test.as','P='+prefix)
+    server.createPV(prefix, pvdb)
+    driver = myDriver()
+    # process CA transactions
+    while True:
+        server.process(0.1)
---
+++ example/test.as
@@ -0,0 +1,6 @@
+ASG(fill) {
+    INPA($(P)LEVEL)
+    RULE(1, WRITE){
+        CALC("A<3")
+    }
+}
