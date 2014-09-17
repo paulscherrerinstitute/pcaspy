f14fa06f6e308704c3cfb250505980e59179ead3
xiaoqiangwang <xiaoqiangwang@localhost>
2014-09-17 09:29:06 +0200

    try to mock cas better

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,18 @@ sys.path.insert(0, os.path.abspath('../../'))
 # mock binary modules
 from mock import Mock as MagickMock
 class Mock(MagickMock):
+    def __call__(self, *args, **kwargs):
+        return Mock()
     @classmethod
     def __getattr__(cls, name):
-        return Mock()
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
 MOCK_MODULES = ['_cas','cas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
