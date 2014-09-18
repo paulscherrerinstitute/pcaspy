477b9bb91c0198bf12a381872c8cec5359603008
xiaoqiangwang <xiaoqiangwang@localhost>
2014-09-18 15:26:18 +0200

    a demo how to use setParamStatus explicitly

--- example/alarm_severity.py
+++ example/alarm_severity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pcaspy import Driver, SimpleServer, Severity
+from pcaspy import Driver, SimpleServer, Alarm, Severity
 
 prefix = 'MTEST:'
 pvdb = {
@@ -14,11 +14,28 @@ pvdb = {
         'enums':  ['OK', 'ERROR'],
         'states': [Severity.NO_ALARM, Severity.MAJOR_ALARM]
     },
+    'MSG' : {
+        'type' : 'str',
+    }
 }
 
 class myDriver(Driver):
     def  __init__(self):
         super(myDriver, self).__init__()
+
+    def write(self, reason, value):
+        status = False
+        if reason == 'MSG':
+            status = True
+            # store the value and this also resets alarm status and severity for string type
+            self.setParam(reason, value)
+            # set alarm status and severity
+            if value != '':
+                self.setParamStatus(reason, Alarm.COMM_ALARM, Severity.MINOR_ALARM)
+            else:
+                self.setParamStatus(reason, Alarm.NO_ALARM, Severity.NO_ALARM)
+
+        return status
 
 if __name__ == '__main__':
     server = SimpleServer()
