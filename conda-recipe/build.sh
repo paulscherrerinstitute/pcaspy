aabea129d34a705c451cd34c9649715b4d4d1d6a
ebner <simon.ebner@psi.ch>
2016-02-22 14:14:30 +0100

    improved build script

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,5 +1,15 @@
 #echo $PREFIX
 export EPICS_BASE=$PREFIX/epics
-export EPICS_HOST_ARCH=darwin-x86 # echo $(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)
+
+PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+if [ $PLATFORM == "linux" ] ; then
+  export EPICS_HOST_ARCH=$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)
+elif [ $PLATFORM == "darwin" ] ; then
+  export EPICS_HOST_ARCH=darwin-x86
+fi
+
+echo Using EPICS_HOST_ARCH=$EPICS_HOST_ARCH
+
+ # echo $(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)
 #$PYTHON setup.py build     # Python command to install the script
 $PYTHON setup.py install     # Python command to install the script
