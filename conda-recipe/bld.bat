038d65c28b3280501645f66cf7f59663ac4d8e03
XiaoqiangWang <xiaoqiangwang@gmail.com>
2016-06-04 20:39:44 +0200

    epics base is not yet for anaconda windows, use external epics base

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,2 +1,12 @@
+REM set EPICS_BASE=%PREFIX:\=/%/epics/
+
+REM if %ARCH%==32 (
+REM    set EPICS_HOST_ARCH=win32-x86
+REM ) else if %ARCH%==64 (
+REM     set EPICS_HOST_ARCH=windows-x64
+REM )
+echo Using EPICS_BASE=%EPICS_BASE%
+echo Using EPICS_HOST_ARCH=%EPICS_HOST_ARCH%
+
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
