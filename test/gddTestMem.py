a019932b2a578efa1d378f9e3759f8c47e0b0dbd
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2016-07-01 08:26:54 +0200

    test gdd.createDD

--- test/gddTestMem.py
+++ test/gddTestMem.py
@@ -2,7 +2,21 @@ from pcaspy.cas import gdd
 import time
 
 while True:
-    d = gdd()
-    d.put(range(1000000))
-    v = d.get()
-    time.sleep(0.2)
+    gddValue = gdd()
+    gddValue.put(range(1000000))
+    v = gddValue.get()
+    gddCtrl = gdd.createDD(34) # gddAppType_dbr_ctrl_double
+    gddCtrl[1].put('eV')
+    gddCtrl[2].put(0)
+    gddCtrl[3].put(1)
+    gddCtrl[4].put(0)
+    gddCtrl[5].put(1)
+    gddCtrl[6].put(0)
+    gddCtrl[7].put(1)
+    gddCtrl[8].put(0)
+    gddCtrl[9].put(1)
+    gddCtrl[10].put(3)
+    gddCtrl[11].put(gddValue)
+
+
+    time.sleep(0.1)
