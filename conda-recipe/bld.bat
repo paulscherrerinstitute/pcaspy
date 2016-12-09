9ea8e2b8b7635bfd13c023254a9db6915d268190
XiaoqiangWang <xiaoqiangwang@gmail.com>
2016-12-09 14:46:55 +0100

    restore conda windows build using epics-base package

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,10 +1,10 @@
-REM set EPICS_BASE=%PREFIX:\=/%/epics/
+set EPICS_BASE=%PREFIX%\epics
 
-REM if %ARCH%==32 (
-REM    set EPICS_HOST_ARCH=win32-x86
-REM ) else if %ARCH%==64 (
-REM     set EPICS_HOST_ARCH=windows-x64
-REM )
+if %ARCH%==32 (
+   set EPICS_HOST_ARCH=win32-x86
+) else if %ARCH%==64 (
+    set EPICS_HOST_ARCH=windows-x64
+)
 echo Using EPICS_BASE=%EPICS_BASE%
 echo Using EPICS_HOST_ARCH=%EPICS_HOST_ARCH%
 
