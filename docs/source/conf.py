83b742a5d5546ff105b8c265e6f49f1c194fd30d
wang_x1 <xiaoqiang.wang@psi.ch>
2017-03-06 12:44:45 +0100

    add C extension _cas to mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ class Mock(MagickMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['cas']
+MOCK_MODULES = ['cas', '_cas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration -----------------------------------------------------
