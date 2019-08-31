e6a63c8412005995eb00a8feb777c6c856786ecd
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2019-08-31 16:20:48 +0200

    link static epics libraries

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,9 +1,9 @@
 set EPICS_BASE=%PREFIX%\epics
 
 if %ARCH%==32 (
-   set EPICS_HOST_ARCH=win32-x86
+   set EPICS_HOST_ARCH=win32-x86-static
 ) else if %ARCH%==64 (
-    set EPICS_HOST_ARCH=windows-x64
+    set EPICS_HOST_ARCH=windows-x64-static
 )
 echo Using EPICS_BASE=%EPICS_BASE%
 echo Using EPICS_HOST_ARCH=%EPICS_HOST_ARCH%
