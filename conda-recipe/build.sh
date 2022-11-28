9f5793a097a3c3b049b8350056a51d6ad8e05e12
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2022-11-28 11:44:21 +0100

    support macOS on arm64 (darwin-aarch64)

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,10 +2,18 @@
 export EPICS_BASE=$PREFIX/epics
 
 PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+MACHINE=$(uname -m)
 if [ $PLATFORM == "linux" ] ; then
-  export EPICS_HOST_ARCH=$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)
+  export EPICS_HOST_ARCH=linux-$MACHINE
 elif [ $PLATFORM == "darwin" ] ; then
-  export EPICS_HOST_ARCH=darwin-x86
+  if [ $MACHINE == "arm64" ]; then
+    export EPICS_HOST_ARCH=darwin-aarch64
+  elif [ $MACHINE == "x86_64" ]; then
+    export EPICS_HOST_ARCH=darwin-x86
+  else
+    echo "macOS CPU type '$MACHINE' not recognized"
+    exit 1
+  fi
 fi
 
 echo Using EPICS_BASE=$EPICS_BASE
