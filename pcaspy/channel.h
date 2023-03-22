d3599995788ee358619fe0e449dc3567330f92f3
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2023-03-22 16:13:44 +0100

    mark overrided methods virtual

--- pcaspy/channel.h
+++ pcaspy/channel.h
@@ -16,8 +16,8 @@ class Channel : public casChannel {
         /* server library calls these methods to determine
          * client's access rights.
          */
-        bool readAccess() const;
-        bool writeAccess() const;
+        virtual bool readAccess() const;
+        virtual bool writeAccess() const;
     private:
         Channel & operator = ( const Channel & );
         Channel ( const Channel & );
